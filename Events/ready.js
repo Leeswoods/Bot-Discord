24b1a2bd7b712a7c831b635020679398d458ddf8
Damien Ouvrard <ouvrard.damien33@gmail.com>
2023-01-17 00:07:22 +0100

    Base de donnée xampp MYSQL

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,15 @@
 const Discord = require("discord.js");
+// Importer fichier LoadDataBase
+const loadDatabase = require("../Loaders/loadDatabase");
 const loadSlashCommands = require("../Loaders/loadSlashCommands");
 
 module.exports = async (bot) => {
+  // Base de donnée
+  bot.db = await loadDatabase();
+
+  bot.db.connect(function () {
+    console.log("Base de donnée connectée avec succès !");
+  });
   await loadSlashCommands(bot);
   console.log(`${bot.user.tag} est bien en ligne !`);
 };
